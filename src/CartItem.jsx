69fbe786714c87f9e4ad2dabb76838a5f6a2f7b3
HeedlessGentry <chrisgithinji286@gmail.com>
2024-11-25 08:29:10 -0500

    Continue from Task 3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,14 +9,18 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return items.reduce((total, item) => {
+        return total + item.quantity * item.cost;
+    },0);
   };
 
   const handleContinueShopping = (e) => {
-   
+    
+   };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
-
-
 
   const handleIncrement = (item) => {
   };
@@ -57,7 +61,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
